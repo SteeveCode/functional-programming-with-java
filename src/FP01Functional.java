4843cd69312f269ef3290545fd08a01ed14bd671
steevecode <stevecode@yahoo.com>
2023-08-08 12:33:06 +0100

    Functional Programming - Improve Java Functional Program with filter

--- src/FP01Functional.java
+++ src/FP01Functional.java
@@ -1,22 +1,26 @@
 import java.util.List;
+import java.util.function.Predicate;
 
-//Problem 1: Traditional Approach Print each element from a List on its own line
-
+//Problem 1: Functional Approach Print each element from a List on its own line
+//Problem 1: Functional Approach Print even numbers from a List on its own line
 public class FP01Functional {
     public static void main(String[] args) {
-        printAllNumberInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
-    }
-    private static void print(int number){
-        System.out.println(number);
+//      printAllNumberInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        printEvenNumberInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+
     }
     private static void printAllNumberInListFunctional(List<Integer> numbers){
 //      What to do?
         numbers.stream()
-                .forEach(FP01Functional::print); // Method Reference = className + :: + static methodName
-
-//      How to loop the numbers and what to do?
-//        for(int number:numbers){
-//            System.out.println(number);
-//        }
+                .forEach(System.out::println); // Method Reference = className + :: + static methodName
+    }
+    private static void printEvenNumberInListFunctional(List<Integer> numbers){
+//      What to do?
+        numbers.stream()
+                .filter(FP01Functional::isEven) //  Filter - Only allow even numbers
+                .forEach(System.out::println); // Method Reference = className + :: + static methodName
+    }
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
--- src/FP01Structured.java
+++ src/FP01Structured.java
@@ -1,16 +1,26 @@
 import java.util.List;
 
 //Problem 1: Traditional Approach Print each element from a List on its own line
+//Problem 1: Traditional Approach Print even numbers from a List on its own line
 
 public class FP01Structured {
     public static void main(String[] args) {
-        printAllNumberInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+//      printAllNumberInListStructured(numbers);
+        printEvenNumberInListStructured(numbers);
+
     }
     private static void printAllNumberInListStructured(List<Integer> numbers){
-//        How to loop the numbers?
-
+//      How to loop the numbers and then what to do?
         for(int number:numbers){
             System.out.println(number);
+        }
+    }
+    private static void printEvenNumberInListStructured(List<Integer> numbers){
+        for(int number:numbers){
+            if(number % 2 == 0)
+                System.out.println(number);
         }
     }
 }
